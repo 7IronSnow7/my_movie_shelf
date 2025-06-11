9d86f988f7eee5deed44f87e55c9e0b15a2f9696
7IronSnow7 <divan.debruin25@gmail.com>
2025-06-11 18:37:57 -0400

    Initialised the movie module in models for variable structure

--- models/movie.py
+++ models/movie.py
@@ -1,6 +1,22 @@
- # What we need for a movie 
- # title ? string
- # year ? int
- # rating ? int
+from datetime import datetime, timezone
+from models import db
 
- 
+class Movie(db.Model):
+    __tablename__ = 'movies'
+    
+    # Primary key
+    id = db.Column(db.Integer, primary_key=True)
+    
+    # Columns
+    title = db.Column(db.String(100), nullable=False)
+    year = db.Column(db.Integer, nullable=True) 
+    watched = db.Column(db.Boolean, default=False, nullable=False)
+    imdb_rating = db.Column(db.Float, nullable=True) # 7.8, 8.2 etc
+    poster_url = db.Column(db.String(200), nullable=True)
+    plot = db.Column(db.String(1000), nullable=True)
+    imdb_id = db.Column(db.String(20), nullable=True)
+    user_rating = db.Column(db.Integer, nullable=True) # Scale 1 to 10
+    notes = db.Column(db.String(500), nullable=True) 
+    date_added = db.Column(db.DateTime, default=datetime.now(timezone.utc))
+    
+        
