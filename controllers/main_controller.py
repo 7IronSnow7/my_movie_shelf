5209281be294bd3264d9015148b208bb706c8a78
7IronSnow7 <divan.debruin25@gmail.com>
2025-06-13 18:14:30 -0400

    Updated the UI/UX for the homepage to let the user know how many movies they have in ther list, how many they have watched and how many the still need to watch

--- controllers/main_controller.py
+++ controllers/main_controller.py
@@ -1,7 +1,16 @@
 from flask import Blueprint, render_template
+from models.movie import Movie
 
 main_bp = Blueprint('main', __name__)
 
 @main_bp.route('/')
 def home():
-    return render_template('index.html')
+    
+    total_movies = Movie.query.count()
+    watched_movies = Movie.query.filter_by(watched=True).count()
+    unwatched_movies = Movie.query.filter_by(watched=False).count()
+    
+    return render_template('index.html',
+                           total_movies=total_movies,
+                           watched_movies=watched_movies,
+                           unwatched_movies=unwatched_movies)
